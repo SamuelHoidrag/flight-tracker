5858372f4706f52f5ca592b122410f24f84260b8
Sergiu Samuel Hoidrag <Sergiu-Samuel.Hoidrag@endava.com>
2022-03-13 22:19:17 +0200

    Test commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from './components/layoutMain/Layout'
 import FormFlight from './components/formFlight/FormFlight'
 
 function App() {
+  // Test comment
   return (
     <>
       <Layout />
